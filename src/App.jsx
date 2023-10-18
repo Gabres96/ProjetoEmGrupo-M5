2c847dc25ce4050ad3ed04369fb13ad744cfccfd
Gabres96 <gabriel.rodrigues670@gmail.com>
2023-10-17 22:43:23 -0300

    feat: criação da rota contrato

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home/Home'
-import Cliente from './pages/Cliente/Cliente'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import Cliente from "./pages/Cliente/Cliente";
 
 function App() {
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/cliente'  element ={ <Cliente/>}/>
-    </Routes>
-   </BrowserRouter>
-  
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cliente" element={<Cliente />} />
+        <Route path="/contrato" element={<Contrato />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
