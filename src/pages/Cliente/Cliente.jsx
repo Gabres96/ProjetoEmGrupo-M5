ea267d73c529c222b541943016b51851de77c416
Gabres96 <gabriel.rodrigues670@gmail.com>
2023-10-17 22:19:23 -0300

    feat: importação do cliente formlário na página cliente

---
+++ src/pages/Cliente/Cliente.jsx
@@ -0,0 +1,10 @@
+import ClienteFormulario from "../../components/ClienteFormulario/ClienteFormulario"
+
+
+const Cliente = () => {
+  return (
+    <ClienteFormulario/>
+  )
+}
+
+export default Cliente
