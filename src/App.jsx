6d4cf3a8c6dfcb0ff471422101964ba6f6a61e9c
Gabres96 <gabriel.rodrigues670@gmail.com>
2023-10-17 22:18:08 -0300

    feat: Criação da rota cliente

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
+import Cliente from './pages/Cliente/Cliente'
+
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/cliente'  element ={ <Cliente/>}/>
     </Routes>
    </BrowserRouter>
+  
   )
 }
 
