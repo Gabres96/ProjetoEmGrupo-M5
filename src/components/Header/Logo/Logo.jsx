9467002cc444c72fdc947dc8bc9b82129850b820
SuayMack <pry_chu@hotmail.com>
2023-10-18 00:24:17 -0300

    feat: criação da logo

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,7 +1,32 @@
-export const Logo = () => {
+import styled from "styled-components";
+
+const Logo = () => {
   return (
-    <div>
-      <img src="./src/assets/logo.jpg" alt="" />
-    </div>
-  )
-}
+    <StyleLogo>
+      <img className={"logo"} src="./src/assets/logo.jpg" alt="" />
+      <h1>DataDynamo</h1>
+    </StyleLogo>
+  );
+};
+
+export default Logo;
+
+const StyleLogo = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  padding: 30px;
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+
+  h1{
+    font-family: "Montserrat", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    color: ${(props) => props.theme.white};
+  }
+`;
