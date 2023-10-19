24bf53aeed598e5f97dc4f7ba6ce604879725700
SuayMack <pry_chu@hotmail.com>
2023-10-18 22:34:20 -0300

    refactor: ajuste no tamanho da logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,6 @@ const Logo = () => {
   return (
     <StyleLogo>
       <img className={"logo"} src="./src/assets/logo.jpg" alt="" />
-      <h1>DataDynamo</h1>
     </StyleLogo>
   );
 };
@@ -12,21 +11,11 @@ const Logo = () => {
 export default Logo;
 
 const StyleLogo = styled.div`
+  background-color: ${(props) => props.theme.header};
   display: flex;
-  position: fixed;
-  top: 0;
-  padding: 30px;
-
   img {
     width: 100px;
-    height: 100px;
   }
 
-  h1{
-    font-family: "Montserrat", sans-serif;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-    color: ${(props) => props.theme.white};
-  }
+  
 `;
