be0139502cef69c324602dab98172340585c400a
SuayMack <pry_chu@hotmail.com>
2023-10-21 13:45:57 -0300

    feat: criação do componente Cliente

---
+++ src/components/Cliente/Cliente.jsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+const Cliente = ({ id, nome, telefone, email, cnpj, endereco, handleEditarCLiente, handleAbrirModalDelete }) => {
+
+  return (
+    <StylesCliente>
+      <table>
+        <thead>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Nome</td>
+            <td>{nome}</td>
+          </tr>
+
+          <tr>
+            <td>Fone</td>
+            <td>{telefone}</td>
+          </tr>
+          <tr>
+            <td>E-mail</td>
+            <td>{email}</td>
+          </tr>
+          <tr>
+            <td>CNPJ</td>
+            <td>{cnpj}</td>
+          </tr>
+          <tr>
+            <td>Endereço</td>
+            <td>{endereco}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button onClick={() => handleEditarCliente({ id, nome, email, cnpj, endereco })}>Editar</button>
+      <button onClick={() => handleAbrirModalDelete(id)} >Excluir</button>
+    </StylesCliente>
+  )
+
+}
+  
+
+export default Cliente
+
+const StylesCliente = styled.li`
+  width: 100%;
+  padding: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+  background-color: ${props => props.theme.white50};;
+
+  > .valor{
+    font-weight: bold;
+  }
+`
