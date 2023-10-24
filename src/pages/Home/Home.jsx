55685a82a1014d2fe79fc107a95c3592f1c97b98
SuayMack <pry_chu@hotmail.com>
2023-10-24 20:04:07 -0300

    style: ajuste nos componentes da página home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
+import { Contratos } from "../Contratos/Contratos";
 
 export const Home = () => {
 
   return (
     <>    
       <Header/>
+      <Contratos/>
     </>
   )
 }
