720d5cf878a620e1def138b5d8679155edc53d39
SuayMack <pry_chu@hotmail.com>
2023-10-24 20:08:05 -0300

    refactor: ajuste do conflito da página Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,17 @@
 import Header from "../../components/Header/Header";
-<<<<<<< HEAD
 import { Contratos } from "../Contratos/Contratos";
 
-=======
 import Clientes from "../Clientes/Clientes";
 import Footer from "../../components/Footer/Footer";
->>>>>>> 3f1576fb1a9f5f1e4557e595b4bbfc7c93dff153
 export const Home = () => {
 
   return (
     <>    
       <Header/>
-<<<<<<< HEAD
       <Contratos/>
-=======
       <Clientes/>
       <Footer/>
 
->>>>>>> 3f1576fb1a9f5f1e4557e595b4bbfc7c93dff153
     </>
   )
 }
