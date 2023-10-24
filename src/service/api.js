c37985838bb7d77393a8f4b66ac0286cdea30e93
SuayMack <pry_chu@hotmail.com>
2023-10-23 22:53:29 -0300

    feat: criação da integração com api para login dos funcionarios

--- src/service/api.js
+++ src/service/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: 'https://apidatadynamo.onrender.com/',
+  // baseURL: 'http://localhost:3000/',
   headers: {
     'Content-type': 'application/json',
   }
@@ -9,14 +10,12 @@ const api = axios.create({
 
 export const getClientes = async () => {
   const resposta = await api.get('/cliente')
-  console.log(resposta.data)
   return (resposta.data)
 }
 
 export const postCliente = async (body) => {
   try {
     const resposta = await api.post('/cliente/criar', body)
-    console.log(resposta.data)
     return resposta.data
   } catch (error) {
     if (error.response) {
@@ -67,4 +66,14 @@ export const deleteCliente = async (idCliente) => {
     }
   }
 }
+
+export const loginUsuario = async (payload) => {
+  try {
+    await api.post('/login', payload)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
 export default api
