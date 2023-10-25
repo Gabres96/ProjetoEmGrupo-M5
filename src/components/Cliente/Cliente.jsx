a5bb135ed434a2b59463ff1e773e62bb1ec1dc9d
Gabres96 <gabriel.rodrigues670@gmail.com>
2023-10-25 19:53:17 -0300

    refactor: troca da largura dos cards e troca da cor

--- src/components/Cliente/Cliente.jsx
+++ src/components/Cliente/Cliente.jsx
@@ -44,14 +44,14 @@ const Cliente = ({ id, nome, telefone, email, cnpj, endereco, handleEditarClient
 export default Cliente
 
 const StylesCliente = styled.li`
-  width: 500px;
+  width: 544px;
   padding: 15px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
   border-radius: 8px;
-  background-color: ${props => props.theme.white50};;
+  background-color: ${props => props.theme.gray50};;
   >.btn {
     display: flex;
     flex-direction: column;
